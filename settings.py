6d96871111f76470e13876c2c6a7ce5e828103b3
ccx2333 <102203673+ccx2333@users.noreply.github.com>
2024-03-25 19:39:50 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = " sk-EN3nOiIv55DRMZZU0rS4H8fU4QPw94ml0Om4PJrYC2M0YDel"
+OPENAI_API_KEY = " EN3nOiIv55DRMZZU0rS4H8fU4QPw94ml0Om4PJrYC2M0YDel"
 
 # openAi 官方 api
 URL = "https://api.openai.com/v1/chat/completions"
